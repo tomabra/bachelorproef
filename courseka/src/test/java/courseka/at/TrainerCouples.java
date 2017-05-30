e1e1d90b8302e576c42563736d59abb68e9f5a5d
tomabra <tom.abrahams@cegeka.com>
2017-05-30 14:45:45 +0200

    bachelorproef

---
+++ courseka/src/test/java/courseka/at/TrainerCouples.java
@@ -0,0 +1,10 @@
+package courseka.at;
+
+import net.serenitybdd.jbehave.SerenityStory;
+
+/**
+ * trainer_couples.story -> TrainerCouples.java
+ */
+
+public class TrainerCouples extends SerenityStory {
+}
